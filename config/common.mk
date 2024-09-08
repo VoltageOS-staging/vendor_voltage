34a8a779083165a1ab4aeccae86a14e3a6e9065f
Sourajit Karmakar <sourajit.karmakar@gmail.com>
2024-09-08 11:33:52 -0400

    statix: Fix up aconfig pick up behavior
    Change-Id: I809f2eb7232c1e3755a50467341bdb50a6ec3c7f
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -5,6 +5,8 @@ PRODUCT_BRAND ?= VoltageOS
 
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
 
+include vendor/gapps/products/gapps.mk
+
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     dalvik.vm.debug.alloc=0 \
     keyguard.no_require_sim=true \
