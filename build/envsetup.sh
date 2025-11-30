risingOSS/vendor_lineage

build: Add `privateremote` helper
Change-Id: I8f406d397914836ac1ac12a50f56263cfcf85b55

build/envsetup.sh
@@ -322,6 +322,22 @@ function githubremote()
     echo "Remote 'github' created"
 }
 
+function privateremote()
+{
+    if ! git rev-parse --git-dir &> /dev/null
+    then
+        echo ".git directory not found. Please run this from the root directory of the Android repository you wish to set up."
+        return 1
+    fi
+    git remote rm private 2> /dev/null
+    local REMOTE=$(git config --get remote.github.projectname)
+
+    local PROJECT=$(echo $REMOTE | sed -e "s#https://github.com/##g")
+
+    git remote add private git@github.com:$PROJECT.git
+    echo "Remote 'private' created"
+}
+
 function installboot()
 {
     if [ ! -e "$OUT/recovery/root/system/etc/recovery.fstab" ];

VoltageOS-staging/vendor_voltage

build/envsetup.sh
@@ -265,6 +265,20 @@ function githubremote()
     echo "Remote 'github' created"
 }
 
+function privateremote()
+{
+    if ! git rev-parse --git-dir &> /dev/null
+    then
+        echo ".git directory not found. Please run this from the root directory of the Android repository you wish to set up."
+        return 1
+    fi
+    git remote rm private 2> /dev/null
+    local PROJECT=$(git config --get remote.github.projectname)
+
+    git remote add private git@github.com:$PROJECT.git
+    echo "Remote 'private' created"
+}
+
 function installboot()
 {
     if [ ! -e "$OUT/recovery/root/system/etc/recovery.fstab" ];