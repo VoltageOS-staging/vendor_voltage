LineageOS/android_vendor_lineage

kernel: Wipe bazel dist dir on each build
Unless you are using copy_to_dist_dir which is deprecated you cannot
use --wipe_dist_dir, so must be done manually.

Change-Id: I1dc2d8a404d292d1a10ed12faadd523076ace305

build/tasks/kernel.mk
@@ -764,6 +764,7 @@ endif
 $(TARGET_PREBUILT_INT_KERNEL): $(DEPMOD) $(KERNEL_MODULES_PARTITION_FILE_LIST) $(SYSTEM_KERNEL_MODULES_PARTITION_FILE_LIST)
 	@echo "Building $(BOARD_KERNEL_IMAGE_NAME)"
 	@mkdir -p $(KERNEL_OUT) $(KERNEL_BAZEL_OUT)
+	$(hide) rm -f $(KERNEL_OUT)
 	$(hide) cd $(KERNEL_PATH) && \
 		python3 $(BUILD_TOP)/.repo/repo/repo manifest -o - -r \
 		| awk -v pat="kernel/platform/kernel-$(TARGET_KERNEL_VERSION)" ' \

VoltageOS-staging/vendor_voltage

Signed-off-by: Dmitrii <bankersenator@gmail.com>

build/tasks/kernel.mk
@@ -769,6 +769,7 @@ endif
 
 $(TARGET_PREBUILT_INT_KERNEL): $(DEPMOD) $(KERNEL_MODULES_PARTITION_FILE_LIST) $(SYSTEM_KERNEL_MODULES_PARTITION_FILE_LIST)
 	@echo "Building $(BOARD_KERNEL_IMAGE_NAME)"
+	$(hide) rm -rf $(KERNEL_OUT)
 	@mkdir -p $(KERNEL_OUT) $(KERNEL_BAZEL_OUT)
 	$(hide) cd $(KERNEL_PATH) && \
 		python3 $(BUILD_TOP)/.repo/repo/repo manifest -o - -r \